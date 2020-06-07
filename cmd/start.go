d4938c3c112cd65e8d57b4cf241d3fd5fba4a463
Dan Esparza <esparza.dan@gmail.com>
2020-06-07 17:26:48 -0400

    Updates to start command

--- cmd/start.go
+++ cmd/start.go
@@ -1,36 +1,108 @@
 package cmd
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
 
+	"github.com/danesparza/fxaudio/api"
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // startCmd represents the start command
 var startCmd = &cobra.Command{
 	Use:   "start",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Start the API and UI services",
+	Long:  `Start the API and UI services`,
+	Run:   start,
+}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("start called")
-	},
+func start(cmd *cobra.Command, args []string) {
+
+	//	If we have a config file, report it:
+	if viper.ConfigFileUsed() != "" {
+		log.Println("[DEBUG] Using config file:", viper.ConfigFileUsed())
+	}
+
+	//	Create our 'sigs' and 'done' channels (this is for graceful shutdown)
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	//	Indicate what signals we're waiting for:
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
+
+	//	Create an api service object
+	apiService := api.Service{StartTime: time.Now()}
+
+	//	Create a router and setup our REST endpoints...
+	UIRouter := mux.NewRouter()
+
+	//	UI ROUTES
+	if viper.GetString("uiservice.ui-dir") == "" {
+		//	Use the static assets file generated with
+		//	https://github.com/elazarl/go-bindata-assetfs using the application-monitor-ui from
+		//	https://github.com/danesparza/application-monitor-ui.
+		//
+		//	To generate this file, run `yarn build` under the "navajo-plex-ui" project.
+		//	Then rename the 'build' directory to 'ui', place that
+		//	directory under the main navajo-plex directory and run the commands:
+		//	go-bindata-assetfs -pkg cmd -o .\cmd\bindata.go ./ui/...
+		//	go install ./...
+
+		//  UIRouter.PathPrefix("/ui").Handler(http.StripPrefix("/ui", http.FileServer(assetFS())))
+	} else {
+		//	Use the supplied directory:
+		log.Printf("[INFO] Using UI directory: %s\n", viper.GetString("uiservice.ui-dir"))
+		UIRouter.PathPrefix("/ui").Handler(http.StripPrefix("/ui", http.FileServer(http.Dir(viper.GetString("uiservice.ui-dir")))))
+	}
+
+	//	SERVICE ROUTES
+	//	-- Log data
+	UIRouter.HandleFunc("/v1/play", apiService.PlayAudio).Methods("GET") // play audio
+
+	//	Setup the CORS options:
+	log.Printf("[INFO] Allowed CORS origins: %s\n", viper.GetString("uiservice.allowed-origins"))
+
+	uiCorsRouter := cors.New(cors.Options{
+		AllowedOrigins:   strings.Split(viper.GetString("uiservice.allowed-origins"), ","),
+		AllowCredentials: true,
+	}).Handler(UIRouter)
+
+	//	Format the bound interface:
+	formattedUIInterface := viper.GetString("uiservice.bind")
+	if formattedUIInterface == "" {
+		formattedUIInterface = "127.0.0.1"
+	}
+
+	//	Start our shutdown listener (for graceful shutdowns)
+	go func() {
+		//	If we get a signal...
+		_ = <-sigs
+
+		//	Indicate we're done...
+		done <- true
+	}()
+
+	//	Start the API and UI services
+	go func() {
+		log.Printf("[INFO] Starting Management UI: http://%s:%s/ui/\n", formattedUIInterface, viper.GetString("uiservice.port"))
+		log.Printf("[ERROR] %v\n", http.ListenAndServe(viper.GetString("uiservice.bind")+":"+viper.GetString("uiservice.port"), uiCorsRouter))
+	}()
+
+	//	Wait for our signal and shutdown gracefully
+	<-done
+
+	log.Printf("[INFO] Shutting down ...")
+
 }
 
 func init() {
 	rootCmd.AddCommand(startCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }

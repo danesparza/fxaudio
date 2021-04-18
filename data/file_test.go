ec042ac11b3b4173a0f753a77c6c96106881ef49
Dan Esparza <esparza.dan@gmail.com>
2021-04-18 11:47:54 -0400

    Add unit test for DeleteFile

--- data/file_test.go
+++ data/file_test.go
@@ -76,3 +76,44 @@ func TestFile_GetAllFiles_ValidFiles_Successful(t *testing.T) {
 	}
 
 }
+
+func TestFile_DeleteFile_ValidFiles_Successful(t *testing.T) {
+
+	//	Arrange
+	systemdb := getTestFiles()
+
+	db, err := data.NewManager(systemdb)
+	if err != nil {
+		t.Fatalf("NewManager failed: %s", err)
+	}
+	defer func() {
+		db.Close()
+		os.RemoveAll(systemdb)
+	}()
+
+	testFile1 := data.File{FilePath: "crossbones1.mp3", Description: "Unit test 1 file"}
+	testFile2 := data.File{FilePath: "crossbones2.mp3", Description: "Unit test 2 file"}
+	testFile3 := data.File{FilePath: "crossbones3.mp3", Description: "Unit test 3 file"}
+
+	//	Act
+	db.AddFile(testFile1.FilePath, testFile1.Description)
+	newFile2, _ := db.AddFile(testFile2.FilePath, testFile2.Description)
+	db.AddFile(testFile3.FilePath, testFile3.Description)
+	db.DeleteFile(newFile2.ID) //	Delete the 2nd file
+
+	gotFiles, err := db.GetAllFiles()
+
+	//	Assert
+	if err != nil {
+		t.Errorf("GetAllFiles - Should get all files without error, but got: %s", err)
+	}
+
+	if len(gotFiles) != 2 {
+		t.Errorf("DeleteFile failed: Should remove an item but got: %v", len(gotFiles))
+	}
+
+	if gotFiles[1].Description == newFile2.Description {
+		t.Errorf("DeleteFile failed: Should get an item with different details than the removed item but got: %+v", gotFiles[1])
+	}
+
+}

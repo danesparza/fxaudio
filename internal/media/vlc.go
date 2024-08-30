381060a13b3d04f05bf0e50cea411ff9464137be
Dan Esparza <danesparza@cagedtornado.com>
2024-08-29 20:48:59 -0400

    Add start of new vlc audio service

---
+++ internal/media/vlc.go
@@ -0,0 +1,43 @@
+package media
+
+import (
+	"context"
+)
+
+type VLCAudioService interface {
+	PlayAudio(ctx context.Context, loop bool, audioFilePath string) error
+	StreamAudio(ctx context.Context, audioStreamUrl string) error
+}
+
+type vlcAudioService struct {
+	//	alsaDevice is the system device to use when playing audio.
+	// 	To list devices, use the command `aplay -L | grep sysdefault`
+	//	If this is blank, the system default device will be used
+	//	and the --alsa-audio-device flag will not be used.
+	//	Using the system default device may be necessary for playback with certain devices like this:
+	//	https://learn.adafruit.com/adafruit-speaker-bonnet-for-raspberry-pi/overview
+	alsaDevice string
+}
+
+func (v vlcAudioService) PlayAudio(ctx context.Context, loop bool, audioFilePath string) error {
+	//	cvlc --play-and-exit -A alsa --alsa-audio-device sysdefault:CARD=sndrpihifiberry /var/lib/fxaudio/uploads/map1.mp3
+	//	to loop, use the --loop flag.  Example: cvlc --play-and-exit --loop -A alsa /var/lib/fxaudio/uploads/map1.mp3
+
+	//TODO implement me
+	panic("implement me")
+}
+
+func (v vlcAudioService) StreamAudio(ctx context.Context, audioStreamUrl string) error {
+	//	cvlc --play-and-exit -A alsa --alsa-audio-device sysdefault:CARD=sndrpihifiberry http://ice1.somafm.com/u80s-128-mp3
+	//	It's so simple!
+
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewVLCAudioService(alsaDevice string) VLCAudioService {
+	svc := &vlcAudioService{
+		alsaDevice: alsaDevice,
+	}
+	return svc
+}

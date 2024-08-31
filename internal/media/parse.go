cf65d99f34420395aba3f0c4f1d2f72216410f58
Dan Esparza <danesparza@cagedtornado.com>
2024-08-30 22:06:47 -0400

    Fix: Don't include blank lines when returning alsa devices

--- internal/media/parse.go
+++ internal/media/parse.go
@@ -5,10 +5,17 @@ import "strings"
 // ParseCliOutput takes CLI output and returns a slice of strings.  Each item
 // in the slice represents a single line of output
 func ParseCliOutput(output string) []string {
+	lines := make([]string, 0)
 	retval := make([]string, 0)
-
 	if len(strings.TrimSpace(output)) > 0 {
-		retval = strings.Split(output, "\n")
+		lines = strings.Split(output, "\n")
+	}
+
+	//	Iterate through each entry.  If there is a blank line, remove it
+	for _, item := range lines {
+		if len(strings.TrimSpace(item)) > 0 {
+			retval = append(retval, item)
+		}
 	}
 
 	return retval

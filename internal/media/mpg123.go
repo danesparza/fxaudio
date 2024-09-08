ecc947ee34ef84905ef1f41517fe685307850e1b
Dan Esparza <danesparza@cagedtornado.com>
2024-09-07 21:21:06 -0400

    Revert "Experiment with paplay"
    This reverts commit 8273c393c220cfa6b3326f6eb892fae089397ba9.

--- internal/media/mpg123.go
+++ internal/media/mpg123.go
@@ -18,16 +18,16 @@ func (a audioService) PlayAudio(ctx context.Context, loop bool, audioPathOrUrl s
 	args := []string{}
 
 	//	If we need to loop, indicate that we should
-	//if loop {
-	//	args = append(args, "--loop", "-1")
-	//}
+	if loop {
+		args = append(args, "--loop", "-1")
+	}
 
 	//	At the end, add the file to play or url to stream
 	args = append(args, audioPathOrUrl)
 
 	//	Finally, run the full command:
 	log.Info().Strs("args", args).Msg("Playing audio")
-	err := exec.CommandContext(ctx, "paplay", args...).Start()
+	err := exec.CommandContext(ctx, "mpg123", args...).Start()
 	if err != nil {
 		log.Err(err).Strs("args", args).Msg("Problem playing audio")
 		return fmt.Errorf("problem playing audio: %w", err)
